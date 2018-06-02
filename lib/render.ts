b2c254e558f35ea57147bd2623740a9f63998e96
Vitaly Arkhipov <arvitaly@list.ru>
2018-06-02 07:08:44 +0300

    temporary disable real hydrate

--- lib/render.ts
+++ lib/render.ts
@@ -13,18 +13,6 @@ export function replace(el: Component<any>, to: HTMLElement) {
 export function hydrate(el: Component<any>, to: HTMLElement) {
     el.mount();
     const root = to.childNodes[0];
-    morphdom(root, el.getRootElement(), {
-        onBeforeElUpdated: (oldEl: Element, newEl: any) => {
-            if (newEl._events) {
-                newEl._events.map((event: any) => {
-                    oldEl.addEventListener(event.eventName, event.listener, event.bubbles);
-                });
-            }
-            if (newEl.onUpdateElement) {
-                newEl.onUpdateElement(oldEl);
-            }
-            return true;
-        },
-    });
-
+    to.replaceChild(el.getRootElement(), root);
+    // TODO: MAKE REAL HYDRATE
 }
